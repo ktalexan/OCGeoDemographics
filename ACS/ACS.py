12ae453784822861d961e9e13f50f49c0dac13e2
Kostas Alexandridis <ktalexan@outlook.com>
2021-09-09 08:13:43 -0700

    Initial Commit ACS19

--- ACS/ACS.py
+++ ACS/ACS.py
@@ -2,29 +2,35 @@ import os, arcpy
 
 #year = 2017
 #est = 5
-year = 2018
+#year = 2018
+#est = 5
+year = 2019
 est = 5
-
 
 prefix = f"ACS{year}Y{est}"
 #prjDir = r"E:\Dev\Repos\OCGeoDemographics\ACS"
-prjDir = r"C:\Users\ktale\Source\Repos\ktalexan\OCGeoDemographics\ACS"
+#prjDir = r"C:\Users\ktale\Source\Repos\ktalexan\OCGeoDemographics\ACS"
+prjdir = r"C:\Users\ktalexan\Documents\GitHub\OCGeoDemographics\ACS"
 #dataOrigin = r"E:\ACS2017"
-dataOrigin = r"D:\OneDrive - County of Orange\Projects\OCGeodemographics\OCACS\OCACS2018"
+#dataOrigin = r"D:\OneDrive - County of Orange\Projects\OCGeodemographics\OCACS\OCACS2018"
+dataOrigin = r"C:\Users\ktalexan\OneDrive - County of Orange\Projects\OCGeodemographics\OCACS\OCACS2019"
 
 os.chdir(prjDir)
 
 from ocacs import ocacs
 
 #acs17 = ocacs(year, est, dataOrigin, prjDir)
-acs18 = ocacs(dataOrigin)
+#acs18 = ocacs(dataOrigin)
+acs19 = ocacs(dataOrigin)
 
-acs18.acsCreateGdbStructure()
-acs18.acsAddGdbAlias()
-
+#acs18.acsCreateGdbStructure()
+#acs18.acsAddGdbAlias()
 
 #acs17.acsCreateGdbStructure()
 #acs17.acsAddGdbAlias()
+
+acs19.acsCreateGdbStructure()
+acs19.acAddGdbAlias()
 
 
 xlsxMetadata = os.path.join(prjDir, "Metadata and Documentation", "MasterMetadata.xlsx")
