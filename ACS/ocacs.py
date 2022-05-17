da56c7ecf947485e2ad2483cb866c61c573d9e3c
Kostas Alexandridis <ktalexan@outlook.com>
2022-05-17 13:21:03 -0700

    beautification

--- ACS/ocacs.py
+++ ACS/ocacs.py
@@ -16,7 +16,7 @@ from numpy import isinimport, pandas
 class ocacs(object):
     """
     OCACS Python Class. Processes and analyzes census data for the American Community Survey products for the geographies in Orange County, California.
-
+    
     Args:
         Inputs:
             year(integer): The ACS year.
@@ -44,29 +44,29 @@ class ocacs(object):
         Credits:
             Script created by Dr. Kostas Alexandridis, GISP, OC Public Works, OC Survey/Geospatial Services, December 2019-2022, Version 2.0
     """
-
-
-###### Function: Class Initialization ######
-
+    
+    
+    ###### Function: Class Initialization ######
+    
     def __init__(self, prjPath):
         """Class instantiation function. See class notes.
-
+        
         Args:
             prjPath(path): the path to the project directory.
         """
-
+        
         # Creates path for the "original" and "processed" versions of the dataset.
         self.dataIn = os.path.join(prjPath, "Original")
         self.dataOut = os.path.join(prjPath, "Processed")
-
+        
         # Read the geodatabases, and obtain the Census product, the year, and the year-estimates
         arcpy.env.workspace = self.dataIn
         arcpy.env.overwriteOutput = True
-
+        
         # Read the file geodatabase workspaces in the input folder, and obtain a list of geodatabase names
         self.wrkspListIn = arcpy.ListWorkspaces("ACS*", "FileGDB")
         self.gdbListIn = [os.path.split(w)[1] for w in self.wrkspListIn]
-
+        
         # Get all the prefixes and geo-levels from the geodatabase list
         # table of product list (e.g., ACS)
         prodList = [i.split(".gdb")[0].split("_")[0] for i in self.gdbListIn]
@@ -76,7 +76,7 @@ class ocacs(object):
         estList = [i.split(".gdb")[0].split("_")[2] for i in self.gdbListIn]
         # table of the geographic level (one of 14 geographies)
         geoList = [i.split(".gdb")[0].split("_")[3] for i in self.gdbListIn]
-
+        
         # Calculate and check counts (to make sure uniquiness), and fill relevant data
         if prodList.count(prodList[0]) == len(prodList):
             self.prod = f"{prodList[0]}"
@@ -85,10 +85,10 @@ class ocacs(object):
                 if estList.count(estList[0]) == len(estList):
                     self.est = estList[0]
                     self.prefix = f"OC{self.prod}{self.year}"
-
+                    
         # Compute the US Congress number based on the year of the ACS survey and
         cdn = int(113 + (self.year - 2012) * 0.5)
-
+        
         # Geo-reference lookup table
         self.geolookup = {
             "COUNTY": ("CO", "Orange County"),
@@ -106,10 +106,10 @@ class ocacs(object):
             "BG": ("BG", "Block Groups"),
             "TRACT": ("TR", "Census Tracts")
         }
-
+        
         # Dictionary containing codes and aliases for each of the project's geography levels
         self.geoNames = {}
-
+        
         if "COUNTY" in geoList:
             self.geoNames["CO"] = "Orange County"
         if "COUSUB" in geoList:
@@ -138,10 +138,10 @@ class ocacs(object):
             self.geoNames["BG4"] = "Block Groups"
         if "TRACT" in geoList:
             self.geoNames["Census Tracts"]
-
+            
         # List of all the geographic code levels
         self.geoLevels = [key for key in self.geoNames.keys()]
-
+        
         # List of four ACS category characteristics
         self.acsCategory = {
             "D": "Demographic Characteristics",
@@ -149,33 +149,33 @@ class ocacs(object):
             "E": "Economic Characteristics",
             "H": "Housing Characteristics"
         }
-
+        
         # Close initialization Function
         return super().__init__()
-
+    
     ################ FUNCTION: PROCESSING GEODATABASE TABLES ################
-
+    
     def processTables(self):
         """Processing Geodatabase Tables Function
         """
-
+        
         ################ STEP 1 ################
-
+        
         print("\nSTEP 1: RESTRUCTURING ORIGINAL GEODATABASES")
-
+        
         # setting up the arcpy environment (data-in)
         arcpy.env.workspace = self.dataIn
         arcpy.env.overwriteOutput = True
-
+        
         print(f"\tDefining a list of new geodatabase geographies...")
-
+        
         # List of geodatabases to be created
         gdbListOut = {level: f"{self.prefix}{level}.gdb" for level in self.acsCategory}
-
+        
         # Create new geodatabases for census geographies (delete if exist)
         if os.path.exists(self.dataOut) is False:
             os.mkdir(self.dataOut)
-
+            
         print(f"\tCreating new geodatabases for ACS geographies")
         for level, gdb in gdbListOut.items():
             print(f"\t\tCategory: {self.acsCategory[level]}")
@@ -185,99 +185,99 @@ class ocacs(object):
                 arcpy.Delete_management(pathGdb)
             print(f"\t\t...Greating geodatabase: {gdb}")
             arcpy.CreateFileGDB_management(self.dataOut, gdb)
-
+            
         ################ STEP 2 ################
-
+        
         print(f"\nSTEP 2: CREATING BASE GEOGRAPHIES IN GEODATABASE DIRECTORY")
-
+        
         # Changing the arcpy environment (data-out)
         arcpy.env.workspace = self.dataOut
         arcpy.env.overwriteOutput = True
-
+        
         # Starting with the COUNTY geodatabase
         print("\tSetting-up geography for County level:")
-
+        
         # Create a temporary layer for the original County data (national)
         print("\t\tDefining workspace")
         curWorkspace = [str for str in self.wrkspListIn if "COUNTY" in os.path.split(str)[1]][0]
-
+        
         # Change again the arcpy environment (current workspace)
         print("\t\tSetting-up arcpy environment")
         arcpy.env.workspace = curWorkspace
         arcpy.env.overwriteOutput = True
-
+        
         # Create feature class layer in memory
         fcIn = arcpy.ListFeatureClasses()[0]
         arcpy.MakeFeatureLayer_management(fcIn, "lyrIn")
-
+        
         # Select only the Orange County polygon
         print("\t\tSelecting polygon for Orange County...")
         lyrOut = arcpy.SelectLayerByAttribute_management("lyrIn", "NEW_SELECTION", "GEOID= '06059'")
-
+        
         # Place the resulting Orange County polygon into each of the category geodatabases
         print("\t\tPlacing resulting polygon into geodatabases...")
         for cat in gdbListOut.keys():
             outGdbLyr = os.path.join(self.dataOut, gdbListOut[cat], f"{self.prefix}CO{cat}")
             arcpy.CopyFeatures_management(lyrOut, outGdbLyr)
-
+            
         # Delete the temporary layers
         print("\t\tDeleting temporary layers...")
         arcpy.Delete_management("lyrIn", "lyrOut")
-
+        
         # Add feature class alias
         print("\t\tAdding feature class alias: ")
         for cat in gdbListOut.keys():
             outGdbLyr = os.path.join(self.dataOut, gdbListOut[cat], f"{self.prefix}CO{cat}")
             arcpy.AlterAliasName(outGdbLyr, f"{self.acsCategory[cat]} for {self.geoNames['CO']}")
-
+            
             # Create a feature layer for the new County Layers - will not delete this until the end:
             arcpy.MakeFeatureLayer_management(outGdbLyr, "lyrCounty")
-
+            
             # Remaining geographic levels Processing
             print("\tSetting-up remaining geographies (looping):")
             workspaceOut = os.path.join(self.dataOut, gdbListOut[cat])
-
+            
             # loop through the items in the geolookup table
             for level, (code, alias) in self.geolookup.items():
                 if "COUNTY" not in level:
                     print(f"\n\t\tProcessing level {code}: {alias}")
                     curWorkspace = [str for str in self.wrkspListIn if level in os.path.split(str)[1]][0]
-
+                    
                     print(f"\t\t\tNew geodatabase feature class: {outGdbLyr}")
                     print(f"\t\t\tSetting up arcpy environment...")
                     arcpy.env.workspace = curWorkspace
                     arcpy.env.overwriteOutput = True
-
+                    
                     fcIn = arcpy.ListFeatureClasses()[0]
                     print(f"\t\t\tFeature classes to be copied: {fcIn}")
-
+                    
                     # Create a temporary layer for the original data (national)
                     print(f"\t\t\tCreating temporary layer...")
                     arcpy.MakeFeatureLayer_management(fcIn, "lyrIn")
-
+                    
                     # Select features from original that are within a distance (-1,000 feet) of the Orange County polygon (all are selected)
                     print("\t\t\tSelecting features with distance (1,000 feet) from County layer...")
                     lyrOut = arcpy.SelectLayerByLocation_management("lyrIn", "WITHIN_A_DISTANCE", "lyrCounty", "-1000 Feet", "NEW_SELECTION", "NOT_INVERT")
-
+                    
                     # Place the selected features into the new feature class in the project geodatabases
                     print("\t\t\tCopying selected features to the new geodatabase feature class...")
                     arcpy.CopyFeatures_management(lyrOut, outGdbLyr)
-
+                    
                     # Delete the temporary layers
                     print("\t\t\tDeleting temporary layers...")
                     arcpy.Delete_management("lyrIn", "lyrOut")
-
+                    
                     # Add feature class aliases
                     print(f"\t\t\tAdding feature class alias: {alias}")
                     arcpy.AlterAliasName(outGdbLyr, f"{self.acsCategory[cat]} for {alias}")
-
+                    
             # Delete County feature layer after geoprocessing operations
             print("\n\tDeleting temporary layer for County")
             arcpy.Delete_management("lyrCounty")
-
+            
         # Get the table variables
         tableLevels = self.getTableVars()
-
+        
         # Dictionary containing the ACS Census tables for each of the variable Groups
         tableMatch = {
             "D": {"D01": "X01", "D02": "X01", "D03": "X02", "D04": "X02", "D05": "X03", "D06": "X05"},
@@ -285,47 +285,47 @@ class ocacs(object):
             "E": {"E01": "X23", "E02": "X23", "E03": "X08", "E04": "X08", "E05": "X08", "E06": "X08", "E07": "X08", "E08": "X24", "E09": "X24", "E10": "X24", "E11": ["X19", "X22"], "E12": ["X19", "X20"], "E13": "X19", "E14": "X27", "E15": "X17", "E16": "X17", "E17": "X17", "E18": "X17", "E19": "X17"},
             "H": {"H01": "X25", "H02": "X25", "H03": "X25", "H04": "X25", "H05": "X25", "H06": "X25", "H07": "X25", "H08": "X25", "H09": "X25", "H10": "X25", "H11": "X25", "H12": "X25", "H13": "X25", "H14": "X25", "H15": "X25", "H16": "X25", "H17": "X25", "H18": "X25", "H19": "X25", "H20": "X25", "H21": "X25", "H22": "X25", "H23": "X25"}
         }
-
+        
         ################ STEP 3 ################
-
+        
         print(f"\nSTEP 3: MERGING CENSUS TABLE VARIABLES WITH GEODATABASE GEOGRAPHIES BY CHARACTERISTIC")
-
+        
         # For each category looping
         for cat in gdbListOut.keys():
-
+            
             # Get the matching ACS tables for each of the category groups
             dlist = tableMatch[cat]
-
+            
             # Loop through the variables in each group
             for level, (code, alias) in self.geolookup.items():
                 print(f"\nProcessing level {code}: {alias}")
-
+                
                 # input and output folders
                 inWorkspace = [str for str in self.wrkspListIn if level in os.path.split(str)[1]][0]
                 outWorkspace = os.path.join(self.dataOut, gdbListOut[cat])
-
+                
                 # Name of feature class in the working geodatabase
                 fc = os.path.join(f"{prefix}{code}{cat}")
-
+                
                 # the fields for which the join will be based (first is the geography feature, second is the joined table)
                 joinField1 = "GEOID_Data"
                 joinField2 = "GEOID"
-
+                
                 print("\tSetting-up arcpy environment to input...")
                 arcpy.env.workspace = inWorkspace
                 arcpy.env.overwriteOutput = True
-
+                
                 print("\tObtaining original list of tables...")
                 inTables = arcpy.ListTables("X*")
-
+                
                 print("\tChanging arcpy environment to output...")
                 arcpy.env.workspace = outWorkspace
                 arcpy.env.overwriteOutput = True
-
+                
                 # Looping through each category group and ACS table:
                 for dno, tno, in dlist.items():
                     print(f"\n\tProcessing group {dno} for table(s) {tno}")
-
+                    
                     # if there is only a single table associated with the group:
                     if isinstance(tno, str):
                         # for each ACS table associated
@@ -339,7 +339,7 @@ class ocacs(object):
                                 arcpy.JoinField_management(fc, joinField1, joinTable, joinField2, levelfields)
                                 # Get the fields in the feature class after the join
                                 fcfields = [fcfield.name for fcfield in arcpy.listFields(fc)]
-
+                                
                                 print(f"\t...Adding aliases to feature class for {tno}")
                                 # Getting the alias list for the category group
                                 aliases = [alias for alias in tableLevels[cat][dno].items()]
@@ -351,7 +351,7 @@ class ocacs(object):
                                     if field[1:3] == tno[1:] and field in fcfields:
                                         print(f"\t\t...adding alias {count} of {na} for {dno}: {alias}")
                                         arcpy.AlterField_management(fc, field, field, f"{dno}: {alias}")
-
+                                        
                     # Else, if there is more than one table associated with the group
                     elif isinstance(tno, list):
                         # for each group table
@@ -367,7 +367,7 @@ class ocacs(object):
                                     arcpy.JoinField_management(fc, joinField1, joinTable, joinField2, levelfields)
                                     # Get the fields in the feature class after the join
                                     fcfields = [fcfield.name for fcfield in arcpy.ListFields(fc)]
-
+                                    
                                     print(f"\t...Adding aliases to feature class for {t}")
                                     # Getting the alias list for the category group
                                     aliases = [alias for alias in tableLevels[cat][dno].items()]
@@ -379,10 +379,9 @@ class ocacs(object):
                                         if field[1:3] == t[1:] and field in fcfields:
                                             print(f"\t\t...adding alias {count} of {na} for {dno}: {alias}")
                                             arcpy.AlterField_management(fc, field, field, f"{dno}: {alias}")
-
-
+                                            
     ################ FUNCTION: OBTAIN DATA TABLE VARIABLES ################
-
+    
     def getTableVars(self):
         """ Obtaining Table Data Variables and Aliases
         """
