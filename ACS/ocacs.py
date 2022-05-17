9c31c3dbf7d7fe9058a3123795dbef71d0ee4147
Kostas Alexandridis <ktalexan@outlook.com>
2022-05-17 12:35:47 -0700

    Class initialization function

--- ACS/ocacs.py
+++ ACS/ocacs.py
@@ -44,3 +44,111 @@ class ocacs(object):
         Credits:
             Script created by Dr. Kostas Alexandridis, GISP, OC Public Works, OC Survey/Geospatial Services, December 2019-2022, Version 2.0
     """
+
+
+###### Function: Class Initialization ######
+
+    def __init__(self, prjPath):
+        """Class instantiation function. See class notes.
+
+        Args:
+            prjPath(path): the path to the project directory.
+        """
+
+        # Creates path for the "original" and "processed" versions of the dataset.
+        self.dataIn = os.path.join(prjPath, "Original")
+        self.dataOut = os.path.join(prjPath, "Processed")
+
+        # Read the geodatabases, and obtain the Census product, the year, and the year-estimates
+        arcpy.env.workspace = self.dataIn
+        arcpy.env.overwriteOutput = True
+
+        # Read the file geodatabase workspaces in the input folder, and obtain a list of geodatabase names
+        self.wrkspListIn = arcpy.ListWorkspaces("ACS*", "FileGDB")
+        self.gdbListIn = [os.path.split(w)[1] for w in self.wrkspListIn]
+
+        # Get all the prefixes and geo-levels from the geodatabase list
+        # table of product list (e.g., ACS)
+        prodList = [i.split(".gdb")[0].split("_")[0] for i in self.gdbListIn]
+        # table of the year(s)
+        yearList = [i.split(".gdb")[0].split("_")[1] for i in self.gdbListIn]
+        # table of the estimates (e.g., 1% or 5%)
+        estList = [i.split(".gdb")[0].split("_")[2] for i in self.gdbListIn]
+        # table of the geographic level (one of 14 geographies)
+        geoList = [i.split(".gdb")[0].split("_")[3] for i in self.gdbListIn]
+
+        # Calculate and check counts (to make sure uniquiness), and fill relevant data
+        if prodList.count(prodList[0]) == len(prodList):
+            self.prod = f"{prodList[0]}"
+            if yearList.count(yearList[0]) == len(yearList):
+                self.year = int(yearList[0])
+                if estList.count(estList[0]) == len(estList):
+                    self.est = estList[0]
+                    self.prefix = f"OC{self.prod}{self.year}"
+
+        # Compute the US Congress number based on the year of the ACS survey and
+        cdn = int(113 + (self.year - 2012) * 0.5)
+
+        # Geo-reference lookup table
+        self.geolookup = {
+            "COUNTY": ("CO", "Orange County"),
+            "COUSUB": ("CS", "County Subdivisions"),
+            "PLACE": ("PL", "Cities/Places"),
+            "ZCTA": ("ZC, ZIP Code Tabluation Areas"),
+            "CD": ("CD", f"Congressional Districts of the {cdn}th US Congress"),
+            "SLDL": ("LL", "State Assembly Legislative Districts"),
+            "SLDU": ("UL", "State Senate Legislative Districts"),
+            "SDE": ("ED", "Elementary School Districts"),
+            "SDS": ("SD", "Secondary School Districts"),
+            "SDU": ("UD", "Unified School Districts"),
+            "UA": ("UA", "Urban Areas"),
+            "PUMA": ("PU", "Public Use Microdata Areas"),
+            "BG": ("BG", "Block Groups"),
+            "TRACT": ("TR", "Census Tracts")
+        }
+
+        # Dictionary containing codes and aliases for each of the project's geography levels
+        self.geoNames = {}
+
+        if "COUNTY" in geoList:
+            self.geoNames["CO"] = "Orange County"
+        if "COUSUB" in geoList:
+            self.geoNames["CS"] = "County Subdivisions"
+        if "PLACE" in geoList:
+            self.geoNames["PL"] = "Cities/Places"
+        if "ZCTA" in geoList:
+            self.geoNames["ZC"] = "ZIP Code Tabulation Areas"
+        if "CD" in geoList:
+            self.geoNames["CD"] = f"Congressional Districts, {cdn}th US Congress"
+        if "SLDL" in geoList:
+            self.geoNames["LL"] = "State Assembly Legislantive Districts"
+        if "SLDU" in geoList:
+            self.geoNames["UL"] = "State Senate Legislative Districts"
+        if "SDE" in geoList:
+            self.geoNames["ED"] = "Elementary School Districts"
+        if "SDS" in geoList:
+            self.geoNames["SD"] = "Secondary School Districts"
+        if "SDU" in geoList:
+            self.geoNames["UD"] = "Unified School Districts"
+        if "UA" in geoList:
+            self.geoNames["UA"] = "Urban Areas"
+        if "PUMA" in geoList:
+            self.geoNames["PU"] = "Public Use Microdata Areas"
+        if "BG" in geoList:
+            self.geoNames["BG4"] = "Block Groups"
+        if "TRACT" in geoList:
+            self.geoNames["Census Tracts"]
+
+        # List of all the geographic code levels
+        self.geoLevels = [key for key in self.geoNames.keys()]
+
+        # List of four ACS category characteristics
+        self.acsCategory = {
+            "D": "Demographic Characteristics",
+            "S": "Social Characteristics",
+            "E": "Economic Characteristics",
+            "H": "Housing Characteristics"
+        }
+
+        # Close initialization Function
+        return super().__init__()
